6f2fe55379cebc5398b7570c1f1db93ae92ee8b0
Paul Bennet <paulbennetjoshua@gmail.com>
2021-08-02 19:46:05 +0530

    enhancement: medal standings fetch added, command args format changed

--- README.md
+++ README.md
@@ -5,19 +5,35 @@ A puppeteer based web scraper for olympics event schedule
 
 ````bash
 npm install -g olympics-tokyo-2021-schedule-scraper
+````
 
-olympics-2021-scraper <date-to-parse> <output-dir>
+````bash
+#Options:
+#  -o --output-dir <output-dir>  Output directory path
+#  -d --date <date-to-parse>     Date to fetch game schedules
+#  -m --medals                   Fetch medal standings
 
-# e.g. olympics-2021-scraper 2021-08-01 ./schedules
+# 1. [SCRAPE] Fetch event schedules
+olympics-2021-scraper --date=<date-to-parse> --out-dir=<output-dir>
 
-# NOTE: <date-to-parse> format must be YYYY-MM-DD
-# JSON files will be written in specified {output-dir} with the name format 'olympics-schedule-{date-to-parse}.json'
+# e.g. olympics-2021-scraper --date=2021-08-01 --out-dir=./schedules
+
+# 2. [SCRAPE] Fetch medal standings
+olympics-2021-scraper --medals --out-dir=<output-dir>
+
+# NOTE:
+# 1. <date-to-parse> format must be YYYY-MM-DD
+# 2. JSON files will be written in specified {output-dir} with the name format 'olympics-schedule-{date-to-parse}.json'
 ````
 
 **Tip:** You can also use this utility without installing the package
 
 ````bash
-npx olympics-tokyo-2021-schedule-scraper <date-to-parse> <output-dir>
+# Fetch event schedules
+npx olympics-tokyo-2021-schedule-scraper --date=<date-to-parse> --out-dir=<output-dir>
+
+# Fetch medal standings
+npx olympics-tokyo-2021-schedule-scraper --date=<date-to-parse> --out-dir=<output-dir>
 ````
 
 
--- index.js
+++ index.js
@@ -2,9 +2,35 @@
 
 const puppeteer = require('puppeteer')
 const fse = require('fs-extra')
-const path = require('path');
+const path = require('path')
+const { program } = require('commander')
 
-(async () => {
+const mainProcess = async () => {
+  program.option('-o --output-dir <output-dir>', 'Output directory path')
+  program.option('-d --date <date-to-parse>', 'Date to fetch game schedules')
+  program.option('-m --medals', 'Fetch medal standings')
+
+  program.parse(process.argv)
+
+  let {
+    date: dateToParse,
+    outputDir,
+    medals: parseMedalStandings
+  } = program.opts()
+
+  if (!dateToParse && !parseMedalStandings) {
+    console.error('Requires --date or --medals param')
+    return
+  }
+
+  if (!outputDir) {
+    console.error('Requires output-dir param')
+    return
+  }
+
+  outputDir = path.resolve(outputDir)
+  fse.mkdirpSync(outputDir)
+
   const browser = await puppeteer.launch({
     // DEBUG: For visually debugging the browser
     // headless: false,
@@ -22,114 +48,144 @@ const path = require('path');
   // DEBUG: Setting UA to latest chrome
   // page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36')
 
-  const dateToParse = process.argv[2]
-  let outputDir = process.argv[3]
+  let pageURL = null
 
-  if (!dateToParse) {
-    console.error('Requires date-to-parse param')
-    await browser.close()
-    return
+  if (dateToParse) {
+    pageURL = `https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/olympic-schedule-and-results-date=${dateToParse}.htm`
+  } else if (parseMedalStandings) {
+    pageURL = 'https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/medal-standings.htm'
   }
 
-  if (!outputDir) {
-    console.error('Requires output-dir param')
-    await browser.close()
-    return
-  }
-
-  outputDir = path.resolve(outputDir)
-  fse.mkdirpSync(outputDir)
-
-  await page.goto(`https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/olympic-schedule-and-results-date=${dateToParse}.htm`, {
+  await page.goto(pageURL, {
     waitUntil: 'networkidle2',
-    // Hopefully page should load before 10 secs
-    timeout: 10000
+    // Hopefully page should load before 20 secs
+    timeout: 20000
   })
 
   await page.waitForTimeout(2000)
 
-  const eventSchedule = await page.evaluate(() => {
-    return new Promise((resolve, reject) => {
-      const $ = window.$
+  let outputJSON = null
 
-      const events = []
-      const getAbsURL = (relURL) => {
-        return $('<a />').attr('href', relURL)[0].href
-      }
+  if (dateToParse) {
+    outputJSON = await page.evaluate(() => {
+      return new Promise((resolve, reject) => {
+        const $ = window.$
 
-      let lastEventStartTime
+        const getAbsURL = (relURL) => {
+          return $('<a />').attr('href', relURL)[0].href
+        }
 
-      $('.clickable-schedule-row').each((index, element) => {
-        const participants = []
+        const events = []
 
-        $(element).find('.schedule-event .col-sm-6:nth(1) .playerTag').each((playerIndex, playerElement) => {
-          const participant = {
-            countryID: $(playerElement).attr('country'),
-            countryName: $(playerElement).find('.country .noc').attr('title'),
-            countryFlag: getAbsURL($(playerElement).find('.country .flag').attr('src')),
-            name: ($(playerElement).find('.name span:nth(1)').text() || $(playerElement).find('.name').text()).replace(/\n/gmi, '')
-          }
+        let lastEventStartTime
 
-          const medal = ($(playerElement).parent().prev('.MedalBoxSmall').find('img').attr('alt') || '').replace('Medal', '').trim().toUpperCase()
+        $('.clickable-schedule-row').each((index, element) => {
+          const participants = []
 
-          if (medal) {
-            participant.medal = medal
-          }
-
-          const score = $(playerElement).closest('.row').find('.resultContainer .result').text().replace(/\n/gmi, '')
-
-          if (score) {
-            participant.score = score
-
-            if ($(playerElement).closest('.row').find('.resultContainer.winner').length) {
-              participant.isWinner = true
+          $(element).find('.schedule-event .col-sm-6:nth(1) .playerTag').each((playerIndex, playerElement) => {
+            const participant = {
+              countryID: $(playerElement).attr('country'),
+              countryName: $(playerElement).find('.country .noc').attr('title'),
+              countryFlag: getAbsURL($(playerElement).find('.country .flag').attr('src')),
+              name: ($(playerElement).find('.name span:nth(1)').text() || $(playerElement).find('.name').text()).replace(/\n/gmi, '')
             }
+
+            const medal = ($(playerElement).parent().prev('.MedalBoxSmall').find('img').attr('alt') || '').replace('Medal', '').trim().toUpperCase()
+
+            if (medal) {
+              participant.medal = medal
+            }
+
+            const score = $(playerElement).closest('.row').find('.resultContainer .result').text().replace(/\n/gmi, '')
+
+            if (score) {
+              participant.score = score
+
+              if ($(playerElement).closest('.row').find('.resultContainer.winner').length) {
+                participant.isWinner = true
+              }
+            }
+
+            participants.push(participant)
+          }
+          )
+
+          let startTime = $(element).find('.schedule-time-data').attr('full-date')
+
+          if (!startTime && $(element).find('.schedule-time').text().replace(/\n/gmi, '').toUpperCase() === 'FOLLOWED BY') {
+            startTime = lastEventStartTime
           }
 
-          participants.push(participant)
+          lastEventStartTime = startTime
+
+          const medalEventType = (
+            $(element).find('.schedule-event .col-sm-6:nth(0) img.medal').attr('title') || ''
+          ).replace('Medal Event', '').trim().toUpperCase()
+
+          const meta = {
+            sportID: $(element).attr('sport'),
+            sportName: $($(element).parents('.schedule-container').parents('div').prev('.schedule-day-header').find('a')[0]).text().replace(/\n/gmi, ''),
+            sportLogo: getAbsURL($(element).parents('.schedule-container').parents('div').prev('.schedule-day-header').find('.sport-icon').attr('src')),
+            status: $(element).find('.schedule-status').text().replace(/\n/gmi, '').toUpperCase(),
+            participants
+          }
+
+          if (medalEventType) {
+            meta.medalType = medalEventType
+          }
+
+          events.push({
+            startTime,
+            location: $(element).find('.schedule-venue').text().replace(/\n/gmi, ''),
+            title: $(element).find('.schedule-event .col-sm-6:nth(0) a').text().replace(/\n/gmi, ''),
+            link: getAbsURL($(element).attr('data-url')),
+            meta
+          })
         }
         )
 
-        let startTime = $(element).find('.schedule-time-data').attr('full-date')
-
-        if (!startTime && $(element).find('.schedule-time').text().replace(/\n/gmi, '').toUpperCase() === 'FOLLOWED BY') {
-          startTime = lastEventStartTime
-        }
-
-        lastEventStartTime = startTime
-
-        const medalEventType = (
-          $(element).find('.schedule-event .col-sm-6:nth(0) img.medal').attr('title') || ''
-        ).replace('Medal Event', '').trim().toUpperCase()
-
-        const meta = {
-          sportID: $(element).attr('sport'),
-          sportName: $($(element).parents('.schedule-container').parents('div').prev('.schedule-day-header').find('a')[0]).text().replace(/\n/gmi, ''),
-          sportLogo: getAbsURL($(element).parents('.schedule-container').parents('div').prev('.schedule-day-header').find('.sport-icon').attr('src')),
-          status: $(element).find('.schedule-status').text().replace(/\n/gmi, '').toUpperCase(),
-          participants
-        }
-
-        if (medalEventType) {
-          meta.medalType = medalEventType
-        }
-
-        events.push({
-          startTime,
-          location: $(element).find('.schedule-venue').text().replace(/\n/gmi, ''),
-          title: $(element).find('.schedule-event .col-sm-6:nth(0) a').text().replace(/\n/gmi, ''),
-          link: getAbsURL($(element).attr('data-url')),
-          meta
-        })
-      }
-      )
-
-      resolve(events)
+        resolve(events)
+      })
     })
-  })
+  } else if (parseMedalStandings) {
+    outputJSON = await page.evaluate(() => {
+      return new Promise((resolve, reject) => {
+        const $ = window.$
+
+        const medalStandings = []
+
+        $('#medal-standing-table tbody tr').each((index, countryElem) => {
+          medalStandings.push({
+            countryID: $(countryElem).find('.playerTag').attr('country'),
+            countryName: $(countryElem).find('.playerTag a').text().replace(/\n/gmi, ''),
+            rank: (index + 1),
+            rankByMedalCount: $(countryElem).find('td:nth(6)').text().replace(/[\n ]/gmi, ''),
+            medals: {
+              gold: $(countryElem).find('td:nth(2) a').text().replace(/[\n ]/gmi, ''),
+              silver: $(countryElem).find('td:nth(3) a').text().replace(/[\n ]/gmi, ''),
+              bronze: $(countryElem).find('td:nth(4) a').text().replace(/[\n ]/gmi, '')
+            },
+            totalMedalCount: $(countryElem).find('td:nth(5) a').text().replace(/[\n ]/gmi, '')
+          })
+        })
+
+        resolve(medalStandings)
+      })
+    })
+  }
+
+  let outputFilePath = null
+
+  if (dateToParse) {
+    outputFilePath = `olympics-schedule-${dateToParse}.json`
+  } else if (parseMedalStandings) {
+    outputFilePath = 'olympics-medal-standings.json'
+  }
+
+  fse.writeFileSync(path.join(outputDir, outputFilePath),
+    JSON.stringify(outputJSON, null, 2))
 
   await browser.close()
+}
 
-  fse.writeFileSync(path.join(outputDir, `olympics-schedule-${dateToParse}.json`),
-    JSON.stringify(eventSchedule, null, 2))
-})()
+mainProcess()
